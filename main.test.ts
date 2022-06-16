c0c3798f27c5db809baa273a9126e16d94714ef1
Jackson <jackson.cooper@bentley.com>
2022-06-16 10:37:46 -0700

    Add offset test

--- main.test.ts
+++ main.test.ts
@@ -6,3 +6,11 @@ test('rectangular face from orthogonal pair', () => {
     const actual = FaceConnector.toFace(firstBasis, 4, secondBasis, 2);
     expect(actual).toStrictEqual([[0, 0, 0], [4, 0, 0], [0, 2, 0], [4, 2, 0]]);
 });
+
+test('rectangular face from orthogonal pair with offset', () => {
+    const firstBasis = new Vector(1, 0, 0);
+    const secondBasis = new Vector(0, 1, 0);
+    const offset = new Vector(0, 0, 1);
+    const actual = FaceConnector.toFace(firstBasis, 4, secondBasis, 2, offset);
+    expect(actual).toStrictEqual([[0, 0, 1], [4, 0, 1], [0, 2, 1], [4, 2, 1]]);
+});
