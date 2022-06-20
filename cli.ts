f3ad28ac300e64b1cb2f69b239017da97b764144
Jackson <jackson.cooper@bentley.com>
2022-06-20 16:50:29 -0700

    Import `JobArgs` from package and fetch path for configuration

--- cli.ts
+++ cli.ts
@@ -1,27 +1,27 @@
+import { parse } from 'node:path';
+
 import { readParcel, synchronizeParcel } from './main.js';
 import { FaceRepository } from './main.js';
 import { FaceConnector } from './connector.js';
 
-import { ConnectorRunner } from '@itwin/connector-framework';
+import { ConnectorRunner, JobArgs } from '@itwin/connector-framework';
 import { IModelHost, IModelHostConfiguration } from '@itwin/core-backend';
-
-// TODO: Package must export JobArgs type.
-import { JobArgs } from './node_modules/@itwin/connector-framework/lib/Args.js';
 
 async function cli(): Promise<void> {
     const args = process.argv.slice(2);
     const path = args[0];
+    const context = process.argv[1];
 
-    const directory = '/home/jackson/bentley/connector-demo';
+    const directory = parse(context).dir;
 
     const configuration = new IModelHostConfiguration();
-    configuration.appAssetsDir = directory;
+    configuration.cacheDir = directory;
     await IModelHost.startup(configuration);
 
     const jobArgs = new JobArgs({
       source: 'unit.json',
       stagingDir: directory,
-      dbType: 'standalone',
+      dbType: 'snapshot',
     });
 
     const runner = new ConnectorRunner(jobArgs);
